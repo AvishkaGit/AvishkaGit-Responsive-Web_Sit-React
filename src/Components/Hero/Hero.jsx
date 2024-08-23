f5b872800b2e6f471885c5bf5d964707621f66f6
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-23 20:42:04 +0530

    program
    create program.jsx and program.css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
 
 const Hero = () => {
   return (
@@ -7,7 +9,8 @@ const Hero = () => {
         <div className="hero-text">
             <h1>We Ensure better education for a better world</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis esse vitae eligendi numquam quos dolore voluptates voluptatem soluta veniam education</p>
-            <button className='btn'>Explore more</button>
+            <button className='btn'>Explore more <img src={dark_arrow} alt="" /> <img src="" alt="" /> </button>
+            
         </div>
       
     </div>
