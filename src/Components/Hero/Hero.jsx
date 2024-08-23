8c7cce2638270f3dc25cd21eac101f9dd3ca5a92
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-23 14:54:00 +0530

    hero section
    update

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We Ensure better education for a better world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis esse vitae eligendi numquam quos dolore voluptates voluptatem soluta veniam education</p>
+            <button className='btn'>Explore more</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
