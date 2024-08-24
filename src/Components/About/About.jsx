012ed059731571c8c1ea985724acbe235b597437
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-24 17:41:06 +0530

    Nav bar update
    set use satate and other fuctioin

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className="about-left">
+                <img src={about_img} alt="" className='about-img' />
+                <img src={play_icon} alt="" className='play-icon' />
+            </div>
+            <div className="about-right">
+                <h3>ABOUT UNIVERSITY</h3>
+                <h2>Nurturing Tommorrow's Leader Today</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, soluta eum quae sequi nesciunt, harum perspiciatis saepe hic iste aliquam placeat et ipsa possimus maiores optio excepturi amet accusamus officia doloribus adipisci, deserunt vel voluptatibus?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti accusamus sed, minus consequatur molestiae sequi laborum corporis deserunt qui, voluptatum illo ex eius cupiditate incidunt </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti accusamus sed, minus consequatur molestiae sequi laborum corporis deserunt qui, voluptatum illo ex eius cupiditate incidunt </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
