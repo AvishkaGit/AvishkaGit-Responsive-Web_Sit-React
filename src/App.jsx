2908c9f7b068cfb4bb40dfac7d0c832dcf69246f
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-25 14:10:37 +0530

    contact left finish
    contact left finish

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -21,7 +22,8 @@ const App = () => {
         <Campus />
         <Title subTitle='TESTIMONIAL' title='What Student Says' />
         <Testimonials />
-
+        <Title subTitle='Contact Us' title='Get in Touch' />
+        <Contact />
       </div>
 
     </div>
