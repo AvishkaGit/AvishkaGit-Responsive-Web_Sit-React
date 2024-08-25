eaebf72f083027a2cf2f16a2d1b23e7a4702bdcb
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-25 15:18:07 +0530

    web3Forms
    update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
       const data = await response.json();
   
       if (data.success) {
-        setResult("Form Submitted Successfully");
+        setResult("Successfully");
         event.target.reset();
       } else {
         console.log("Error", data);
